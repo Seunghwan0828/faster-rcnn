a95235c5e2dc5634a4d8cd4cfca238bc1e5c498f
Seunghwan0828 <55007714+Seunghwan0828@users.noreply.github.com>
2023-01-03 20:29:30 +0900

    Update eval.py
    edit

--- eval.py
+++ eval.py
@@ -108,7 +108,7 @@ model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 num_classes = 2
 in_features = model.roi_heads.box_predictor.cls_score.in_features
 model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-model.load_state_dict(torch.load("./weights/faster1/model30.pth"))
+model.load_state_dict(torch.load("./weights/model1.pth"))
 
 
 if __name__ == '__main__':
